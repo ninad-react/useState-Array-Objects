7f58f74deb8bdc52145cca72b2d68b79d3ef8429
ninad-react <157863834+ninad-react@users.noreply.github.com>
2024-03-29 09:51:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import HookCounter from './components/HookCounter'
+import HookCounterFour from './components/HookCounterFour'
+import HookCounterThree from './components/HookCounterThree'
+import HookCounterTwo from './components/HookCounterTwo'
+
+function App() {
+
+  return (
+    <>
+      {/* <HookCounter /> */}
+      {/* <HookCounterTwo /> */}
+      {/* <HookCounterThree /> */}
+      <HookCounterFour />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/HookCounter.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+const HookCounter = () => {
+    const [count, setCount] = useState(0)
+  return (
+    <div>
+        <button onClick={() => setCount(count + 1)}>count {count}</button>
+    </div>
+  )
+}
+
+export default HookCounter
